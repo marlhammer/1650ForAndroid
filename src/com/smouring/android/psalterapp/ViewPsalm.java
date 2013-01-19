676052bc1e72f4fc04c85aee548f8b57a65b7111
Stephen Mouring <smouring@nearinfinity.com>
2013-01-19 14:29:14 -0500

    Copied some old code into the ViewPsalm activity for later porting.

--- src/com/smouring/android/psalterapp/ViewPsalm.java
+++ src/com/smouring/android/psalterapp/ViewPsalm.java
@@ -10,6 +10,25 @@ public class ViewPsalm extends Activity {
   public void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
 
-    setContentView(R.layout.viewpsalm);
+	setContentView(R.layout.viewpsalm);
+
+/*
+int selectedBook  = Integer.parseInt(getIntent().getStringExtra("selectedBook"));
+int selectedPsalm = Integer.parseInt(getIntent().getStringExtra("selectedPsalm"));
+
+TextView psalmDisplay = (TextView)findViewById(R.id.psalmdisplay);
+
+String[] psalmVerses = psalms[selectedPsalm - 1];
+
+StringBuilder psalmText = new StringBuilder();
+for (int i = 0; i < psalmVerses.length; ++i) {
+	psalmText.append(psalmVerses[i]);
+}
+
+psalmDisplay.setText(Html.fromHtml(psalmText.toString()));
+
+psalmDisplay.setMovementMethod(new ScrollingMovementMethod());
+*/
+
   }
 }
