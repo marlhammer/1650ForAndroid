f2774c4a2a6111af5708a21294141a07a945cbdd
Stephen Mouring <smouring@nearinfinity.com>
2013-02-02 21:20:34 -0500

    Fixed some bugs, added a back button to view psalm activity, and fixed color of selector buttons.

--- src/com/smouring/android/psalterapp/ViewPsalm.java
+++ src/com/smouring/android/psalterapp/ViewPsalm.java
@@ -1,33 +1,52 @@
 package com.smouring.android.psalterapp;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.Html;
 import android.text.method.ScrollingMovementMethod;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 /**
  * @author Stephen Mouring
  */
 public class ViewPsalm extends Activity {
+  private int selectedBook = 0;
+  private int selectedPsalm = 0;
+
   public void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
 
     setContentView(R.layout.viewpsalm);
 
-    int selectedBook = Integer.parseInt(getIntent().getStringExtra("selectedBook"));
-    int selectedPsalm = Integer.parseInt(getIntent().getStringExtra("selectedPsalm"));
+    selectedBook = Integer.parseInt(getIntent().getStringExtra("selectedBook"));
+    selectedPsalm = Integer.parseInt(getIntent().getStringExtra("selectedPsalm"));
 
     TextView psalmText = (TextView) findViewById(R.id.psalmtext);
 
     // TODO: Temporary solution until Lucene is working.
     StringBuilder psalmString = new StringBuilder();
-    for (String psalmLine : Constants.PSALMS[selectedPsalm]) {
+    for (String psalmLine : Constants.PSALMS[selectedPsalm - 1]) {
       psalmString.append(psalmLine);
     }
 
     psalmText.setText(Html.fromHtml(psalmString.toString()));
 
     psalmText.setMovementMethod(new ScrollingMovementMethod());
+
+    Button selectButton = (Button) findViewById(R.id.back);
+    selectButton.setOnClickListener(new View.OnClickListener() {
+      public void onClick(View v) {
+        Log.i("1650ForAndroid", "Launching intent for ViewPsalms activity.");
+
+        Intent i = new Intent(ViewPsalm.this, ViewPsalms.class);
+        i.putExtra("selectedBook", String.valueOf(selectedBook));
+        i.putExtra("selectedPsalm", String.valueOf(selectedPsalm));
+        startActivity(i);
+      }
+    });
   }
 }
--- src/com/smouring/android/psalterapp/ViewPsalms.java
+++ src/com/smouring/android/psalterapp/ViewPsalms.java
@@ -43,38 +43,85 @@ public class ViewPsalms extends Activity {
     }
   }
 
-  protected int selectedBook = 0;
-  protected int selectedPsalm = 0;
+// TODO
+//
+//  private boolean applySelections = true;
+
+  private int selectedBook = 1;
+  private int selectedPsalm = 1;
 
   public void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
 
     setContentView(R.layout.viewpsalms);
 
-    System.setProperty("log.tag.1650ForAndroid", "WARN");
+    System.setProperty("log.tag.1650ForAndroid", "INFO");
+
+// TODO
+//
+//    applySelections = false;
+
+    String selectedBookParam = getIntent().getStringExtra("selectedBook");
+    String selectedPsalmParam = getIntent().getStringExtra("selectedPsalm");
+    if (selectedBookParam != null && selectedBookParam.length() > 0) {
+      selectedBook = Integer.parseInt(selectedBookParam);
+// TODO
+//
+//      applySelections = true;
+      Log.i("1650ForAndroid", "Received selectedBook parameter: [" + selectedBook + "]");
+    } else {
+      selectedBook = 1;
+    }
+    if (selectedPsalmParam != null && selectedPsalmParam.length() > 0) {
+      selectedPsalm = Integer.parseInt(selectedPsalmParam);
+// TODO
+//
+//      applySelections = true;
+      Log.i("1650ForAndroid", "Received selectedPsalm parameter: [" + selectedPsalm + "]");
+    } else {
+      selectedPsalm = 1;
+    }
+    Log.i("1650ForAndroid", "selectedBook set to: [" + selectedBook + "]");
+    Log.i("1650ForAndroid", "selectedPsalm set to: [" + selectedPsalm + "]");
 
     Spinner chooseBook = (Spinner) findViewById(R.id.choosebook);
-    ArrayAdapter<String> bookNameAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, BOOK_NAMES);
+    ArrayAdapter<String> bookNameAdapter = new ArrayAdapter<String>(this, R.layout.psalmselector, BOOK_NAMES);
     bookNameAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
     chooseBook.setAdapter(bookNameAdapter);
-//    chooseBook.setSelection(0);
     chooseBook.setOnItemSelectedListener(new OnItemSelectedListenerAdapter() {
       public void onItemSelected(AdapterView parent, View view, int pos, long id) {
+        Log.i("1650ForAndroid", "chooseBook onItemSelectedListener fired.");
+
         selectedBook = Integer.parseInt(parent.getItemAtPosition(pos).toString().split(" ")[1]);
+        Log.i("1650ForAndroid", "selectedBook set to: [" + selectedBook + "]");
 
         Spinner choosePsalm = (Spinner) findViewById(R.id.choosepsalm);
-        ArrayAdapter<String> psalmNameAdapter = new ArrayAdapter<String>(ViewPsalms.this, android.R.layout.simple_list_item_1, PSALM_NAMES[selectedBook - 1]);
+        ArrayAdapter<String> psalmNameAdapter = new ArrayAdapter<String>(ViewPsalms.this, R.layout.psalmselector, PSALM_NAMES[selectedBook - 1]);
         psalmNameAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         choosePsalm.setAdapter(psalmNameAdapter);
+
+// TODO
+//
+//        if (applySelections) {
+//          choosePsalm.setSelection(selectedPsalm);
+//        }
       }
     });
 
+// TODO
+//
+//    if (applySelections) {
+//      chooseBook.setSelection(selectedBook);
+//    }
+
     Spinner choosePsalm = (Spinner) findViewById(R.id.choosepsalm);
-    ArrayAdapter<String> psalmAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, new String[]{""});
+    ArrayAdapter<String> psalmAdapter = new ArrayAdapter<String>(this, R.layout.psalmselector, new String[]{""});
     psalmAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
     choosePsalm.setAdapter(psalmAdapter);
     choosePsalm.setOnItemSelectedListener(new OnItemSelectedListenerAdapter() {
       public void onItemSelected(AdapterView parent, View view, int pos, long id) {
+        Log.i("1650ForAndroid", "choosePsalm onItemSelectedListener fired.");
+
         selectedPsalm = Integer.parseInt(parent.getItemAtPosition(pos).toString().replace("Psalm ", ""));
       }
     });
@@ -82,7 +129,7 @@ public class ViewPsalms extends Activity {
     Button selectButton = (Button) findViewById(R.id.select);
     selectButton.setOnClickListener(new OnClickListener() {
       public void onClick(View v) {
-        Log.e("1650ForAndroid", "Launching intent for ViewPsalm activity.");
+        Log.i("1650ForAndroid", "Launching intent for ViewPsalm activity.");
 
         Intent i = new Intent(ViewPsalms.this, ViewPsalm.class);
         i.putExtra("selectedBook", String.valueOf(selectedBook));
